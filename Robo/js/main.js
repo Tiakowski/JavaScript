4892d11775d92f4126545cdf27b04fae6b6aa40c
Tiakowski <tiagosantosrazr@gmail.com>
2022-06-07 19:45:09 -0300

    Robo

--- Robo/js/main.js
+++ Robo/js/main.js
@@ -1,9 +1,47 @@
 
 const seletor = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 seletor.forEach(( elemento) => {
     elemento.addEventListener("click",(evento) => {
         ManipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-        console.log(elemento)
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
 
@@ -19,4 +57,10 @@ function ManipulaDados(operacao, controle){
             peca.value--
             }
     }
+}
+
+function atualizaEstatisticas(peca) {
+    estatisticas.forEach( (elemento) => {
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
 }
